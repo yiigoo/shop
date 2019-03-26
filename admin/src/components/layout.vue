789ae4e1c3b1854fb2631b31b42c4b5132fa3f12
yiigoo <yiigoo@163.com>
2019-03-27 02:13:36 +0800

    layout

---
+++ admin/src/components/layout.vue
@@ -0,0 +1,57 @@
+<template>
+	<div>
+
+		<el-container>
+
+			<el-header>Header</el-header>
+
+			<el-container>
+
+				<el-aside width="200px">
+					<el-menu 
+						:default-active="activeIndex"
+						background-color="#545c64"
+						text-color="#fff"
+						active-text-color="#ffd04b"
+						class="el-menu-demo" 
+						router="router"
+						@select="handleSelect">
+						<el-menu-item index="/goods">　xx　</el-menu-item>
+						<el-menu-item index="/users">　xx2　</el-menu-item>
+					</el-menu>
+				</el-aside>
+
+				<el-container>
+					<el-main><router-view></router-view></el-main>
+					<el-footer>copyright @2019</el-footer>
+				</el-container>
+
+			</el-container>
+		</el-container>
+	</div>
+</template>
+
+<script>
+	
+	export default {
+		name: 'layout',
+		data() {
+			return {
+				activeIndex : '/goods'
+			}
+		},
+		methods: {
+			handleSelect( n ) {
+
+			}
+		},
+		created() {
+			console.log( this.$router )
+			// $axios( 'admin/cover/img' , { params : { aid : '47059776' } }).then(( data )=>{
+			// 	this.cover = data
+			// }).catch( error =>{
+
+			// })
+		}
+	}
+</script>
