b7b0173b792f7458eb5abb2c28b758a63f1a4edc
pactera-338 <pactera-338>
2019-03-28 16:26:20 +0800

    表格组件封装

---
+++ admin/src/components/siteTable.vue
@@ -0,0 +1,49 @@
+<template>
+	<div>
+		<el-table 
+			:data="tableData" 
+			stripe 
+			style="width: 100%">
+
+			<el-table-column
+				v-for="item in tableColumn" 
+				:key="item.label"
+				:prop="item.prop"
+				:label="item.label">
+					<template slot-scope="scope">
+						<slot :name="item.prop" :scope="scope">
+							{{ scope.row[item.prop] }} 
+						</slot>
+					</template>
+					
+			</el-table-column>
+		</el-table>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Goods',
+		props : {
+			tableColumn : {
+				type : Array ,
+				default : []
+			},
+			tableData : {
+				type : Array ,
+				default : []
+			},
+		},
+		data() {
+			return {
+			}
+		},
+		methods: {},
+		mounted() {
+			
+		},
+		created() {
+			
+		}
+	}
+</script>
