e2748b6363d68233861bb0a4a77a27e848e39d13
pactera-338 <pactera-338>
2019-04-02 10:37:13 +0800

    vant

---
+++ users/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body{
+    line-height: 1;
+    color: #323233;
+    background-color: #fafafa;
+    font-family: 'PingFang SC', Helvetica, 'STHeiti STXihei', 'Microsoft YaHei', Tohoma, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+}
+</style>
---
+++ users/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="hello">
+		{{ currentDate }}
+		<van-datetime-picker
+			v-model="currentDate"
+			type="year-month"
+			:min-date="minDate"
+			:max-date="maxDate"
+			@confirm="confirm"
+			/>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			minHour: 10,
+			maxHour: 20,
+			minDate: new Date(),
+			maxDate: new Date(2019, 10, 1),
+			currentDate: new Date(2019, 10, 1),
+			}
+		},
+		methods: {
+			confirm (value){
+				alert( value )
+			}
+		}
+	}
+</script>
+<style scoped>
+</style>
